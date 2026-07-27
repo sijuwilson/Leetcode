953afc3665d1f5c9c1921ed47cb74a853bbc0112
sijuwilson <sijuwilson07@gmail.com>
2026-07-27 09:16:38 +0530

    problem no 1464 solved

---
+++ Arrays/1464_max_product_of_two_elements_in_an_array.py
@@ -0,0 +1,36 @@
+"""
+    Level: Easy
+    Problem:
+    Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+    Example 1:
+
+    Input: nums = [3,4,5,2]
+    Output: 12 
+    Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+
+    Example 2:
+
+    Input: nums = [1,5,4,5]
+    Output: 16
+    Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+
+    Constraints:
+
+    2 <= nums.length <= 500
+    1 <= nums[i] <= 10^3.
+
+"""
+
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        if nums[0] < nums[1]: i, j = 1, 0
+        else: i, j = 0, 1
+        for k in range(2,len(nums)):
+            if nums[k] > nums[i]:
+                j = i
+                i = k
+            else:
+                j = j if nums[j] > nums[k] else k
+        
+        return (nums[i] - 1) * (nums[j] - 1)
